6a6742f883232a2e44e25241fe2c396597bfee60
Harrison Goldstein <hgoldstein95@gmail.com>
2018-04-03 11:48:11 -0400

    Documentation for Wit library

--- src/Wit.hs
+++ src/Wit.hs
@@ -1,4 +1,17 @@
-module Wit where
+{-|
+The core functionality of <https://wit.ai Wit.ai>, implemented in terms of
+'Parser' and its associated combinators.
+-}
+module Wit (
+  -- * Wit Model
+    WitModel
+  , trainWitModel
+  -- * Search Strategies
+  , keyword
+  , freeText
+  , intent
+  )
+where
 
 import Parsing
 
@@ -8,18 +21,32 @@ import qualified Data.Text.Lazy as T
 
 import NLP.Hext.NaiveBayes
 
+-- | A naive Bayes classifier, using the
+-- <https://hackage.haskell.org/package/hext hext> library.
 type WitModel = BayesModel
 
-trainWitModel :: Ord a => [(String, a)] -> WitModel a
+-- | Trains a model with pairs of strings and their associated label.
+trainWitModel :: Ord label => [(String, label)] -> WitModel label
 trainWitModel = foldl (\m (sample, cl) -> teach (T.pack sample) cl m) emptyModel
 
+-- | Finds some (case insensitive) keyword, anywhere in the input.
 keyword :: String -> Parser String String
 keyword = somewhereP . stringP'
 
-freeText :: BayesModel Bool -> Parser String String
+-- | Finds a substring of the input that has some semantic meaning.
+--
+-- Input examples are of the form
+-- @(\<potential substring>, \<valid free text>)@.
+--
+-- /Note:/ Lots of input examples are needed to make this parser work correctly.
+freeText :: WitModel Bool -> Parser String String
 freeText model = do
   txt <- invisP substringP
   if runBayes model txt then pure txt else empty
 
-intent :: Ord a => BayesModel a -> Parser String a
+-- | Considers the input as a whole and attempts to classify it into some number
+-- of categories.
+--
+-- This can be used to capture things like user intent or mood.
+intent :: Ord label => WitModel label -> Parser String label
 intent model = runBayes model <$> get
